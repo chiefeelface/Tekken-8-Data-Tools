cebe4a92d1a2306185fa1eddb100c16e3aa9bbef
chiefeelface <chiefeelface@gmail.com>
2025-09-28 20:16:47 -0500

    Added choice between sqlite and csv download and made times use utc for consistency

--- src/get_replays.py
+++ src/get_replays.py
@@ -3,22 +3,25 @@ from tqdm import tqdm
 from src.models import ReplayData
 from src.utils import *
 
-START_DATE = datetime.datetime(2025, 9, 1)
-END_DATE = datetime.datetime(2025, 9, 2)
+START_DATE = datetime.datetime(2025, 9, 1).replace(tzinfo=datetime.timezone.utc)
+END_DATE = datetime.datetime(2025, 9, 2).replace(tzinfo=datetime.timezone.utc)
 
-def get_replay_data(start_date: datetime.datetime, end_date: datetime.datetime):
+def get_replay_data(start_date: datetime.datetime, end_date: datetime.datetime, use_sql: bool):
     total_replays: int = 0
     replays: list[ReplayData] = []
+
     start = math.trunc(start_date.timestamp())
-    # Add 1 day and subtract 1 millisecond to end_date since time is midnight
-    end = math.trunc((end_date + datetime.timedelta(days=1) - datetime.timedelta(seconds=1)).timestamp())
+    end = math.trunc((end_date.replace(hour=23,minute=59,second=59)).timestamp())
     now = datetime.datetime.now(datetime.timezone.utc).timestamp()
     if end > now:
         tqdm.write('[Download] | End date has not happened yet, setting to the current time.')
         end = now
-    loops_required = math.ceil((end - start) / 700)
     before = start
-    if config.USE_SQLITE:
+
+    if use_sql:
+        if os.path.exists(config.DB_FILE_BASE_NAME + f'_{start_date.date()}_{end_date.date()}.db'):
+            tqdm.write('[I/O] | Deleting duplicate database file.')
+            os.remove(config.DB_FILE_BASE_NAME + f'_{start_date.date()}_{end_date.date()}.db')
         tqdm.write('[I/O] | Attempting to generate tables with primary and foreign keys.')
         if e := create_tables(start_date, end_date) != None:
             tqdm.write(f'[I/O Error] {e} | Failed to generate tables.')
@@ -29,67 +32,74 @@ def get_replay_data(start_date: datetime.datetime, end_date: datetime.datetime):
             tqdm.write(f'[I/O Error] {e} | Failed to populate helper tables.')
         else:
             tqdm.write(f'[I/O] | Sucessfully populated helper tables.')
-    tqdm.write(f'[Download] | Beginning download of {loops_required:,} sets of replays.')
+
+    loops_required = math.ceil((end - start) / 700)
     loops = 0
-    with tqdm(total=loops_required + 1, desc='Downloading Replay Sets', ncols=100, unit=' replay sets') as progress:
-        while before < end:
-            start_time = time.perf_counter()
+    tqdm.write(f'[Download] | Beginning download of {loops_required:,} sets of replays.')
+    
+    try:
+        with tqdm(total=loops_required + 1, desc='[Download]', ncols=100, unit=' replay sets') as progress:
+            while before < end:
+                start_time = time.perf_counter()
 
-            before += 700
+                before += 700
 
-            try:
-                downloaded = download_replays(before)
-            except KeyboardInterrupt:
-                tqdm.write(f'[Download] | Execution cancelled by user.')
-                total_replays += len(replays)
-                save_replay_data_to_file(replays, start_date, end_date)
-                return total_replays
-            except Exception as e:
-                tqdm.write(f'[Download Error] {e} | Encountered an error while attempting to download set {loops + 1} of {loops_required:,}, retrying.')
-                attempts = 0
-                while attempts < config.MAX_RETRIES:
-                    downloaded = None
-                    try:
-                        time.sleep(1.005)
-                        downloaded = download_replays(before)
-                        tqdm.write(f'[Download Error] | Retry {attempts + 1} succeeded, the set was succesfully downloaded, resuming normal operation.')
-                        break
-                    except Exception as e:
-                        tqdm.write(f'[Download Error] | Retry {attempts + 1} failed, waiting 1 second and trying again.')
-                        attempts += 1
-                else:
-                    tqdm.write(f'[Download Error] | All retry attempts failed for set {loops + 1} of {loops_required:,} with before value {before}, and will not be included in the final output.')
+                try:
+                    downloaded = download_replays(before)
+                except Exception as e:
+                    tqdm.write(f'[Download Error] {e} | Encountered an error while attempting to download set {loops + 1} of {loops_required:,}, retrying.')
+                    attempts = 0
+                    while attempts < config.MAX_RETRIES:
+                        downloaded = None
+                        try:
+                            time.sleep(1.005)
+                            downloaded = download_replays(before)
+                            tqdm.write(f'[Download Error] | Retry {attempts + 1} succeeded, the set was succesfully downloaded, resuming normal operation.')
+                            break
+                        except Exception as e:
+                            tqdm.write(f'[Download Error] | Retry {attempts + 1} failed, waiting 1 second and trying again.')
+                            attempts += 1
+                    else:
+                        tqdm.write(f'[Download Error] | All retry attempts failed for set {loops + 1} of {loops_required:,} with before value {before}, and will not be included in the final output.')
+                    
+                    if downloaded:
+                        replays.extend(downloaded)
+                        total_replays += len(downloaded)
+                        del downloaded [:]
+                    time.sleep(1.005)
+                    continue
                 
-                if downloaded:
-                    replays.extend(downloaded)
-                    total_replays += len(downloaded)
-                    del downloaded [:]
-                time.sleep(1.005)
-                continue
-            
-            replays.extend(downloaded)
-            total_replays += len(downloaded)
-            del downloaded [:]
-            if len(replays) > config.MAX_REPLAY_THRESHOLD:
-                save_replay_data_to_file(replays, start_date, end_date)
-                del replays[:]
-            
-            loops += 1
-            progress.update(1)
-            elapsed_time = time.perf_counter() - start_time 
-            if elapsed_time - 0.005 < 1:
-                time.sleep(1 - elapsed_time + 0.005)
-            
+                replays.extend(downloaded)
+                total_replays += len(downloaded)
+                del downloaded [:]
+                if len(replays) > config.MAX_REPLAY_THRESHOLD:
+                    save_replay_data_to_file(replays, start_date, end_date, use_sql)
+                    del replays[:]
+                
+                loops += 1
+                progress.update(1)
+                elapsed_time = time.perf_counter() - start_time 
+                if elapsed_time - 0.005 < 1:
+                    time.sleep(1 - elapsed_time + 0.005)
+                
+            if replays:
+                save_replay_data_to_file(replays, start_date, end_date, use_sql)
+    except KeyboardInterrupt:
+        tqdm.write('[Download] | Execution interrupted.')
         if replays:
-            save_replay_data_to_file(replays, start_date, end_date)
+            save_replay_data_to_file(replays, start_date, end_date, use_sql)
+        if downloaded:
+            tqdm.write(f'[Download] | Replay set from before value {before} possibly lost.')
+        return total_replays
+
 
     return total_replays
 
-def save_replay_data_to_file(replay_data: list[ReplayData], start_date: datetime.datetime, end_date: datetime.datetime):
+def save_replay_data_to_file(replay_data: list[ReplayData], start_date: datetime.datetime, end_date: datetime.datetime, use_sql: bool):
     tqdm.write(f'[I/O] | Attempting to save {len(replay_data):,} replays to file.')
     try:
         replays_df = pd.DataFrame(replay_data)
-        if config.USE_SQLITE:
+        if use_sql:
             file_name = config.DB_FILE_BASE_NAME + f'_{start_date.date()}_{(end_date).date()}.db'
             # Ensure file exists
             open(file_name, 'a').close()
@@ -112,5 +122,5 @@ def save_replay_data_to_file(replay_data: list[ReplayData], start_date: datetime
 
 if __name__ == '__main__':
     start_time = time.perf_counter()
-    downloaded_replays = get_replay_data(START_DATE, END_DATE)
+    downloaded_replays = get_replay_data(START_DATE, END_DATE, False)
     tqdm.write(f'[Download] | Finished gathering {downloaded_replays:,} replays in a total of {round(time.perf_counter() - start_time, 2):,} seconds')
--- test.py
+++ test.py
@@ -3,31 +3,32 @@ from src.utils import *
 
 class Test(unittest.TestCase):
     def test_replay_download(self):
-        self.assertEqual(len(download_replays(1756684800)), 4482)
+        timestamp = int(datetime.datetime(2025, 9, 1, tzinfo=datetime.timezone.utc).timestamp())
+        self.assertEqual(len(download_replays(timestamp)), 4482)
 
     def test_table_creation(self):
         start, end = datetime.datetime(1970, 1, 1), datetime.datetime(1970, 1, 2)
-        failed = False
+        failed = None
         try:
             self.assertEqual(create_tables(start, end), None)
-        except Exception:
-            failed = True
+        except Exception as e:
+            failed = e
         # Cleanup
-        if os.path.exists(config.DB_FILE_BASE_NAME + f'_{start.date()}_{(end).date()}.db'):
-            os.remove(config.DB_FILE_BASE_NAME + f'_{start.date()}_{(end).date()}.db')
+        if os.path.exists(config.DB_FILE_BASE_NAME + f'_{start.date()}_{end.date()}.db'):
+            os.remove(config.DB_FILE_BASE_NAME + f'_{start.date()}_{end.date()}.db')
         if failed:
-            raise Exception
+            raise failed
         
     def test_table_fill(self):
         start, end = datetime.datetime(1970, 1, 1), datetime.datetime(1970, 1, 2)
-        failed = False
+        failed = None
         try:
             self.assertEqual(fill_tables_for_enums(start, end), None)
-        except Exception:
-            failed = True
-            # Cleanup
-        if os.path.exists(config.DB_FILE_BASE_NAME + f'_{start.date()}_{(end).date()}.db'):
-            os.remove(config.DB_FILE_BASE_NAME + f'_{start.date()}_{(end).date()}.db')
+        except Exception as e:
+            failed = e
+        # Cleanup
+        if os.path.exists(config.DB_FILE_BASE_NAME + f'_{start.date()}_{end.date()}.db'):
+            os.remove(config.DB_FILE_BASE_NAME + f'_{start.date()}_{end.date()}.db')
         if failed:
-            raise Exception
+            raise failed
     
