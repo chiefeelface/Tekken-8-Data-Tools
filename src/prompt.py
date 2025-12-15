1804f29333876851c7f683fcaf1265a5ad04e118
chiefeelface <chiefeelface@gmail.com>
2025-12-14 21:49:00 -0600

    prompt now uses a case statement instead of if-else if statements, allowed exiting out of the program while being asked questions, and added more robust error checking for replay files

--- src/prompt.py
+++ src/prompt.py
@@ -1,55 +1,70 @@
-import src.config as config, questionary as q, datetime, os, pathlib
+import src.config as config, questionary as q, datetime, os
 from src.get_replays import get_replay_data
 from src.analyze_replays import analyze_replay_data
-from src.utils.timer import Timer
 from src.utils.file_utils import write_results_to_excel
 
+def ask_with_interrupt_check(q: q.Question):
+    answer = q.ask()
+    if answer:
+        return answer
+    raise KeyboardInterrupt
+
+def has_replays():
+    return os.path.exists(config.REPLAY_DIR) and os.listdir(config.REPLAY_DIR)
+
 def prompt():
-    action = q.select(
-        message='What would you like to do',
-        choices=[
-            config.DOWNLOAD,
-            config.ANALYZE,
-            config.HELP,
-            config.QUIT
-        ]
-    ).ask()
-    if action == config.DOWNLOAD:
-        start_date = q.text(
-            message='What is the start date to begin downloading replays from (YYYY-MM-DD)',
-            default=datetime.date.today().replace(day=1).strftime('%Y-%#m-%#d')
-        ).ask()
-        end_date = q.text(
-            message='What is the end date to finish downloading replays from (YYYY-MM-DD)',
-            default=datetime.date.today().strftime('%Y-%#m-%#d')
-        ).ask()
-        file_type = q.select(
-            message='What file type would you like the results to be saved to',
-            choices=[
-                config.CSV,
-                config.SQLITE
-            ]
-        ).ask()
-        start_date = datetime.datetime.strptime(start_date, '%Y-%m-%d')
-        end_date = datetime.datetime.strptime(end_date, '%Y-%m-%d')
-        
-        get_replay_data(start_date, end_date, file_type == config.SQLITE)
-    elif action == config.ANALYZE:
-        replay_data_file_path = q.select(
-            message='What file would you like to analyze',
-            choices=[ 
-                file for file in os.listdir(config.REPLAY_DIR)
-            ]
-        ).ask()
-        stats, player_stats, rank_percentiles_and_distribution = analyze_replay_data(config.REPLAY_DIR + '/' + replay_data_file_path)
-        if q.confirm('Would you like to save the results to an excel file').ask():
-            write_results_to_excel(replay_data_file_path, [
-                    (stats, 'Character Stats',),
-                    (player_stats, 'Player Stats',),
-                    (rank_percentiles_and_distribution, 'Rank Percentiles & Distribution',)
-            ])
-    elif action == config.QUIT or action == None:
-        return False
-    elif action == config.HELP:
-        print('🐊')
+    choices = [
+        config.DOWNLOAD,
+        q.Choice(config.ANALYZE, disabled='No Replays Downloaded' if not has_replays() else None),
+        config.HELP,
+        config.QUIT
+    ]
+
+    match q.select(message='What would you like to do', choices=choices).ask():
+        case config.DOWNLOAD:
+            start_date = ask_with_interrupt_check(q.text(
+                message='What is the start date to begin downloading replays from (YYYY-MM-DD)',
+                default=datetime.date.today().replace(day=1).strftime('%Y-%#m-%#d')
+            ))
+            end_date = ask_with_interrupt_check(q.text(
+                message='What is the end date to finish downloading replays from (YYYY-MM-DD)',
+                default=datetime.date.today().strftime('%Y-%#m-%#d')
+            ))
+            file_type = ask_with_interrupt_check(q.select(
+                message='What file type would you like the results to be saved to',
+                choices=[
+                    config.CSV,
+                    config.SQLITE
+                ]
+            ))
+            start_date = datetime.datetime.strptime(start_date, '%Y-%m-%d')
+            end_date = datetime.datetime.strptime(end_date, '%Y-%m-%d')
+            
+            get_replay_data(start_date, end_date, file_type == config.SQLITE)
+        case config.ANALYZE:
+            if not has_replays():
+                print('No replay files found, exiting.')
+                return False
+            replay_data_file_path = ask_with_interrupt_check(q.select(
+                message='What file would you like to analyze',
+                choices=[ 
+                    file for file in os.listdir(config.REPLAY_DIR)
+                ] + [config.BACK]
+            ))
+            if replay_data_file_path == config.BACK:
+                return True
+            stats, player_stats, rank_percentiles_and_distribution = analyze_replay_data(config.REPLAY_DIR + '/' + replay_data_file_path)
+            if ask_with_interrupt_check(q.confirm('Would you like to save the results to an excel file')):
+                write_results_to_excel(replay_data_file_path, [
+                        (stats, 'Character Stats',),
+                        (player_stats, 'Player Stats',),
+                        (rank_percentiles_and_distribution, 'Rank Percentiles & Distribution',)
+                ])
+        case config.QUIT | None:
+            return False
+        case config.HELP:
+            print('🐊 coming soon')
+        case _:
+            print('Unknown option selection, exiting.')
+            return False
     return True
